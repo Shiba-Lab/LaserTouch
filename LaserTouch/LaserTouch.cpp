7fcf30b9034335ed9cb1390191c2cd415ea84d2e
Yucheng Qiu <CitrusC@users.noreply.github.com>
2019-08-17 21:12:12 +0900

    Update LaserTouch.cpp

--- LaserTouch/LaserTouch.cpp
+++ LaserTouch/LaserTouch.cpp
@@ -18,10 +18,12 @@ int main()
 		cerr << "Failed to open property file" << endl;
 		return -1;
 	}
-	int width, height, FPS, aperture, contrast, threshold, minArea, maxArea;
+	int camera_W, camera_H, display_W, display_H, FPS, aperture, contrast, threshold, minArea, maxArea;
 	float inertia;
-	fs["width"] >> width;
-	fs["height"] >> height;
+	fs["camera_W"] >> camera_W;
+	fs["camera_H"] >> camera_H;
+	fs["display_W"] >> display_W;
+	fs["display_H"] >> display_H;
 	fs["FPS"] >> FPS;
 	fs["aperture"] >> aperture;
 	fs["contrast"] >> contrast;
@@ -33,117 +35,131 @@ int main()
 	FileNode fn = fs["calibration"];
 	Point2f src_pt[4];
 	FileNodeIterator it = fn.begin(), it_end = fn.end();
-	for (int id = 0; it != it_end; ++it)
+for (int id = 0; it != it_end; ++it)
+{
+	FileNode item = *it;
+	item >> src_pt[id];
+	++id;
+}
+Point2f dst_pt[] = {
+	Point2f(384, 216),
+	Point2f(1536, 216),
+	Point2f(1536, 864),
+	Point2f(384, 864) };
+Mat h_matrix = getPerspectiveTransform(src_pt, dst_pt);
+
+// Init camera
+VideoCapture camera(0);
+if (!camera.isOpened())
+{
+	printf("Cannot open the USB camera\n");
+	return -1;
+}
+camera.set(CAP_PROP_FOURCC, 'MJPG');
+camera.set(CAP_PROP_FRAME_WIDTH, camera_W);
+camera.set(CAP_PROP_FRAME_HEIGHT, camera_H);
+camera.set(CAP_PROP_FPS, FPS);
+camera.set(CAP_PROP_APERTURE, aperture);
+camera.set(CAP_PROP_CONTRAST, contrast);
+
+printf("Resolution: %.fx%.f  FPS: %.f\n",
+	camera.get(CAP_PROP_FRAME_WIDTH),
+	camera.get(CAP_PROP_FRAME_HEIGHT),
+	camera.get(CAP_PROP_FPS));
+Mat frame;
+Mat gray;
+vector<Mat> channels;
+
+
+// Init blob detector
+SimpleBlobDetector::Params params;
+
+params.minThreshold = threshold;
+params.maxThreshold = 255;
+params.thresholdStep = 30;						// Run threshold only one time
+
+params.filterByArea = true;
+params.minArea = minArea;
+params.maxArea = maxArea;
+
+params.filterByInertia = true;
+params.minInertiaRatio = inertia;
+
+params.filterByCircularity = false;
+//params.minCircularity = 0.1f;
+
+params.filterByConvexity = false;
+//params.minConvexity = 0.87f;
+
+params.filterByColor = true;
+params.blobColor = 255;
+
+vector<KeyPoint> detectPoints;
+int detectCount;
+Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
+
+
+// Init touch input
+vector<TouchInput*> touchPoints;
+int touchCount;
+bool touched[MAX_COUNT] = { 0 };
+// assume a maximum of 10 contacts and turn touch feedback off
+InitializeTouchInjection(MAX_COUNT, TOUCH_FEEDBACK_NONE);
+
+
+printf("Press Esc or Q to stop\n");
+
+
+// Init timer
+//TickMeter tm;
+//tm.reset();
+//tm.start();
+//int frameCount = 0;
+while (camera.read(frame))
+{
+	//cvtColor(frame, gray, COLOR_RGB2GRAY);
+	cv::split(frame, channels);												// Faster than RGB2GRAY when using monochrome camera
+	detector->detect(channels.at(0), detectPoints);
+
+
+	// Perspective correction
+	detectCount = detectPoints.size();
+	vector<Point2f> camera_pt(detectCount);
+	vector<Point2f> display_pt(detectCount);
+	for (int i = 0; i < detectCount; ++i) camera_pt[i] = detectPoints[i].pt;
+	perspectiveTransform(camera_pt, display_pt, h_matrix);
+
+
+	// Remove out of range detect points
+	for (int i = 0; i < detectCount; )
 	{
-		FileNode item = *it;
-		item >> src_pt[id];
-		++id;
+		if (display_pt[i].x < 0 ||
+				display_pt[i].x > display_W ||
+				display_pt[i].y < 0 ||
+				display_pt[i].y > display_H )
+		{
+			display_pt.erase(display_pt.begin() + i);
+			detectCount -= 1;
+			continue;
+		}
+		++i;
 	}
-	Point2f dst_pt[] = {
-		Point2f(384, 216),
-		Point2f(1536, 216),
-		Point2f(1536, 864),
-		Point2f(384, 864) };
-	Mat h_matrix = getPerspectiveTransform(src_pt, dst_pt);
-
-	// Init camera
-	VideoCapture camera(0);
-	if (!camera.isOpened())
-	{
-		printf("Cannot open the USB camera\n");
-		return -1;
-	}
-	camera.set(CAP_PROP_FOURCC, 'MJPG');
-	camera.set(CAP_PROP_FRAME_WIDTH, width);
-	camera.set(CAP_PROP_FRAME_HEIGHT, height);
-	camera.set(CAP_PROP_FPS, FPS);
-	camera.set(CAP_PROP_APERTURE, aperture);
-	camera.set(CAP_PROP_CONTRAST, contrast);
-
-	printf("Resolution: %.fx%.f  FPS: %.f\n", 
-		camera.get(CAP_PROP_FRAME_WIDTH), 
-		camera.get(CAP_PROP_FRAME_HEIGHT), 
-		camera.get(CAP_PROP_FPS));
-	Mat frame;
-	Mat gray;
-	vector<Mat> channels;
 
 
-	// Init blob detector
-	SimpleBlobDetector::Params params;
+	// Track by bounding box
 
-	params.minThreshold = threshold;
-	params.maxThreshold = 255;
-	params.thresholdStep = 30;						// Run threshold only one time
-
-	params.filterByArea = true;
-	params.minArea = minArea;
-	params.maxArea = maxArea;
-
-	params.filterByInertia = true;
-	params.minInertiaRatio = inertia;
-
-	params.filterByCircularity = false;
-	//params.minCircularity = 0.1f;
-
-	params.filterByConvexity = false;
-	//params.minConvexity = 0.87f;
-	
-	params.filterByColor = true;
-	params.blobColor = 255;
-
-	vector<KeyPoint> detectPoints;
-	int detectCount;
-	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
-	
-
-	// Init touch input
-	vector<TouchInput*> touchPoints;
-	int touchCount;
-	bool touched[MAX_COUNT] = { 0 };
-	// assume a maximum of 10 contacts and turn touch feedback off
-	InitializeTouchInjection(MAX_COUNT, TOUCH_FEEDBACK_NONE);
-
-
-	printf("Press Esc or Q to stop\n");
-	
-
-	// Init timer
-	//TickMeter tm;
-	//tm.reset();
-	//tm.start();
-	//int frameCount = 0;
-	while (camera.read(frame))
-	{
-		//cvtColor(frame, gray, COLOR_RGB2GRAY);
-		cv::split(frame, channels);												// Faster than RGB2GRAY when using monochrome camera
-		detector->detect(channels.at(0), detectPoints);
-
-		//std::vector<KeyPoint>::const_iterator it = keypoints.begin(), end = keypoints.end();
-		//for (; it != end & ; ++it)
-		int i = 0;
-		//printf("%d %d\n", (int)touchPoints.size(), (int)detectPoints.size());
-		touchCount = touchPoints.size();
-		detectCount = detectPoints.size();
 
 		for (i = 0; i < detectCount && i < MAX_COUNT; ++i)
 		{
-			// Keystone correction
-			vector<Point2f> camera_pt(1);
-			camera_pt[0] = detectPoints[i].pt;
-			vector<Point2f> display_pt(1);
-			perspectiveTransform(camera_pt, display_pt, h_matrix);
 
-			printf("%f %f %f %f\n", detectPoints[i].pt.x, detectPoints[i].pt.y, display_pt[0].x, display_pt[0].y);
-			
 			if (i < touchCount)
 			{
-				//printf("update\n");
+				// Update touch
 				touchPoints[i]->UpdateInject(i, (int)display_pt[0].x, (int)display_pt[0].y, touchCount);  // ?
-				//touched[i] = true;
 			}
 			else
 			{
+				// New touch
 				//if (0 <= display_pt[0].x && display_pt[0].x <= 1920 && 0 <= display_pt[0].y && display_pt[0].y <= 1080) {
 					printf("init %d\n", i);
 					touchPoints.push_back(new TouchInput(i, (int)display_pt[0].x, (int)display_pt[0].y, touchCount + 1));
@@ -152,10 +168,10 @@ int main()
 			}
 		}
 
-		//for (int i = 0; i < MAX_COUNT; ++i) printf("%d ", touched[i]); 
 		
 		for (int j = touchCount - 1; j >= i; --j)
 		{
+			// Delete touch
 			if (touched[j])
 			{
 				printf("Delete %d\n", j);
@@ -172,8 +188,7 @@ int main()
 		{
 			contacts.push_back(touchPoints[i]->contact);
 		}
-		//printf("count %d %d\n", (int)contacts.size(), (int)touchPoints.size());
-		
+
 		InjectTouchInput(static_cast<UINT32>(contacts.size()), &contacts[0]);
 
 
